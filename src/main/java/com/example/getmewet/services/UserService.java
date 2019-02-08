b96a25b332d06be9cf5beccbf16740d1555621e1
robin-lunde <robin.lunde@linecorp.com>
2019-02-08 11:03:21 +0900

    Functional API sign in

--- src/main/java/com/example/getmewet/services/UserService.java
+++ src/main/java/com/example/getmewet/services/UserService.java
@@ -5,8 +5,6 @@ import com.example.getmewet.models.User;
 import com.example.getmewet.repositories.RoleRepository;
 import com.example.getmewet.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
